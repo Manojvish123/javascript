64498a5e34388d6ebd8f4539097bc8131ea5c857
Manojvish123 <manojvvishwakarma48@gmail.com>
2025-02-16 17:11:49 +0530

    Add files via upload

---
+++ javascript/ind.js
@@ -0,0 +1,16 @@
+// function outer() {
+//     let name = "babber"
+//     function inner() {
+//         console.log(name);
+//     }
+//     return inner;
+// }
+// let num = outer()
+// // num()
+// function clear(){
+//    let clr= document.querySelector("#output")
+//    clr.style.display="none"
+// }
+// function Solve(){
+//     let plus=document.getElementById()
+// }
